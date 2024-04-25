3f48958f693cc81d421bd4022dbf208a03177ed5
Pratap Rathi <prataprathi97@gmail.com>
2024-04-25 12:25:07 +0530

    Initial commit

---
+++ src/app/data.js
@@ -0,0 +1,74 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Airbnb - Clone",
+    description: "Holiday rentals, Cabins, beach, house & more",
+    date: "2024-04-22",
+    demoLink: "https://airbnb-pratap.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Messenger",
+    description: "Realtime messaging app",
+    date: "2024-03-31",
+    demoLink: "https://messenger-pratap.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "NotePlus",
+    description: "Save your personal notes",
+    date: "2023-09-23",
+    demoLink: "https://note-plus.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Weather",
+    description: "Live weather app",
+    date: "2023-08-25",
+    demoLink: "https://prataprathi.github.io/Weather-App/",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/PratapRathi",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/pratap-rathi/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Leetcode",
+    link: "https://leetcode.com/prataprathi97/",
+    icon: "leetcode",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import clsx from "clsx";
+import FireFliesBackground from "@/components/FireFliesBackground";
+import Sound from "@/components/Sound";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+export const metadata = {
+  title: "Pratap Rathi: Crafting Digital Experiences | Full Stack Portfolio",
+  description: "Step into the digital playground of John Doe, where every line of code showcasing a blend of technical prowess and creative flair.",
+  icons: {icon: "/favicon.png"}
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={clsx(
+          inter.variable,
+          "bg-background text-foreground font-inter"
+        )}
+      >
+        {children}
+        <FireFliesBackground />
+        <Sound />
+        <div id="my-modal" />
+      </body>
+    </html>
+  );
+}
